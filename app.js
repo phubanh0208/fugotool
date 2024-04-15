062260b8bf8dcf45bf7a622c9cb3f67d50e3795f
Phu <101729787+phubanh0208@users.noreply.github.com>
2024-04-16 02:03:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const JSZip = require('jszip');
 
 const app = express();
-const port = 3000;
+const port = (process.env.PORT ||3000);
 
 app.use(express.static('public'));
 
